8233af9203b7be5ba70e0aec9602adc6cf39b4f7
Fearocanity <91414643+fearocanity@users.noreply.github.com>
2023-07-12 23:45:50 +0900

    feat: assign vars for timezones

--- frame_main.sh
+++ frame_main.sh
@@ -5,7 +5,7 @@
 [[ -e status/status.jpg ]] && : > status/status.jpg
 
 prev_frame="$(<./fb/frameiterator)"
-time_started="$(TZ='Asia/Tokyo' date)"
+time_started="$(TZ="${sys_timezone}" date)"
 
 # Main Loop
 for ((i=1;i<=fph;i++)); do
@@ -17,7 +17,7 @@ lim_frame="$((prev_frame+fph-1))"
 [[ "${lim_frame}" -gt "${total_frame}" ]] && lim_frame="${total_frame}"
 [[ "${prev_frame}" -gt "${total_frame}" ]] && prev_frame="${total_frame}"
 
-time_ended="$(TZ='Asia/Tokyo' date)"
+time_ended="$(TZ="${sys_timezone}" date)"
 if [[ "${desc_update}" == "1" ]]; then
     ovr_all="$(sed -E ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L' counter_n.txt)"
     abt_txt="$(printf '%s\n%s' "Chopped 2 FPS, Posting ${fph:-??} Frames every 2 hours." "Total of \"${ovr_all:-0}\" frame was successfully posted!!")"
