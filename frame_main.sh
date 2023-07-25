33dfbc56fcfbc1eafade76f715a8735ec0a6e5ff
Fearocanity <91414643+fearocanity@users.noreply.github.com>
2023-07-25 23:13:17 +0900

    feat: page bio formatting template [2]

--- frame_main.sh
+++ frame_main.sh
@@ -20,7 +20,7 @@ lim_frame="$((prev_frame+fph-1))"
 time_ended="$(TZ="${sys_timezone}" date)"
 if [[ "${desc_update}" == "1" ]]; then
     ovr_all="$(sed -E ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L' counter_n.txt)"
-    abt_txt="$(printf '%s\n%s' "Chopped ${img_fps:-??} FPS, Posting ${fph:-??} Frames every 2 hours." "Total of \"${ovr_all:-0}\" frame was successfully posted!!")"
+    abt_txt="$(eval "printf '%s' \"$(sed -E 's_\{\\n\}_\n_g;s_\{([^\x7d]*)\}_\${\1:-??}_g;s|ovr_all:-\?\?|over_all:-0|g' <<< "${abt_txt}"\")")"
     curl -sLk -X POST "https://graph.facebook.com/me/?access_token=${1}" --data-urlencode "about=${abt_txt}" -o /dev/null || true
 fi
 bash img_process.sh "success" "${prev_frame}" "${lim_frame}" "${time_started}" "${time_ended}"
