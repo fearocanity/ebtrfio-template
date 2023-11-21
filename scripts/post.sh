4aae477a6c18367cbaeb04cfbfbd922f91c91380
fearocanity <haloisme45@gmail.com>
2023-11-21 09:45:07 +0900

    chore: appraise the linter

--- scripts/post.sh
+++ scripts/post.sh
@@ -35,7 +35,7 @@ post_fp(){
 		--retry-delay 7 \
 		-F "message=${message}" \
 		-F "source=@${FRMENV_FRAME_LOCATION}/frame_${1}.jpg" \
-		"${FRMENV_API_ORIGIN}/me/photos?access_token=${FRMENV_FBTOKEN}&published=1"
+	"${FRMENV_API_ORIGIN}/me/photos?access_token=${FRMENV_FBTOKEN}&published=1"
 }
 
 post_album(){
@@ -46,7 +46,7 @@ post_album(){
 		-F "source=@${FRMENV_FRAME_LOCATION}/frame_${1}.jpg" \
 		-F "message=${message}" \
 		-o /dev/null \
-		"${FRMENV_API_ORIGIN}/${album}/photos?access_token=${FRMENV_FBTOKEN}&published=1" || return 1
+	"${FRMENV_API_ORIGIN}/${album}/photos?access_token=${FRMENV_FBTOKEN}&published=1" || return 1
 }
 
 post_subs(){
@@ -56,13 +56,13 @@ post_subs(){
 		--retry-delay 7 \
 		--data-urlencode "message=${message_comment}" \
 		-o /dev/null \
-		"${FRMENV_API_ORIGIN}/${FRMENV_FBAPI_VER}/${1}/comments?access_token=${FRMENV_FBTOKEN}" || return 1
+	"${FRMENV_API_ORIGIN}/${FRMENV_FBAPI_VER}/${1}/comments?access_token=${FRMENV_FBTOKEN}" || return 1
 }
 
 post_changedesc(){
 	ovr_all="$(sed -E ':L;s=\b([0-9]+)([0-9]{3})\b=\1,\2=g;t L' counter_n.txt)"
-    get_interval="$(sed -nE 's|.*cron: "[^ ]* \*/([^ ]*) [^ ]* [^ ]*.*"|\1|p' ./.github/workflows/process.yml)"
-    TEMP_ABT_TXT="$(eval "printf '%s' \"$(sed -E 's_\{\\n\}_\n_g;s_\{([^\x7d]*)\}_\${\1:-??}_g;s|ovr_all:-\?\?|ovr_all:-0|g' <<< "${abt_txt}"\")")"
-    curl -sLk -X POST "https://graph.facebook.com/me/?access_token=${1}" --data-urlencode "about=${TEMP_ABT_TXT}" -o /dev/null || true
-    unset TEMP_ABT_TXT 
+	get_interval="$(sed -nE 's|.*cron: "[^ ]* \*/([^ ]*) [^ ]* [^ ]*.*"|\1|p' ./.github/workflows/process.yml)"
+	TEMP_ABT_TXT="$(eval "printf '%s' \"$(sed -E 's_\{\\n\}_\n_g;s_\{([^\x7d]*)\}_\${\1:-??}_g;s|ovr_all:-\?\?|ovr_all:-0|g' <<< "${abt_txt}"\")")"
+	curl -sLk -X POST "${FRMENV_API_ORIGIN}/me/?access_token=${1}" --data-urlencode "about=${TEMP_ABT_TXT}" -o /dev/null || true
+	unset TEMP_ABT_TXT 
 }
