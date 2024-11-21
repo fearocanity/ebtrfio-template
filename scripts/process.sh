66e06a11cde4b625411e68a65a28462fa5458ef0
Fearocanity (けろさん) <91414643+fearocanity@users.noreply.github.com>
2024-11-21 22:21:19 +0900

    fix: multi-subs not posting

--- scripts/process.sh
+++ scripts/process.sh
@@ -126,7 +126,7 @@ process_multisubs(){
 		[[ -e "${i}" ]] || continue
 		[[ "${i}" =~ .*_([A-Za-z]{2})\.(srt|ass|ssa)$ ]] || continue
 		process_subs "${1}" "${i}"
-		[[ -z "${message_comment}" ]] && { unset message_craft BOOL_IS_OPEDSONG ; continue ;}
+		[[ -z "${message_craft}" ]] && { unset message_craft BOOL_IS_OPEDSONG ; continue ;}
 		if [[ "${BOOL_IS_OPEDSONG}" = "1" ]]; then
 			message_comment+="Lyrics [$(sed -E 's/.*_([A-Za-z]{2})\.(srt|ass|ssa)$/\1/g' <<< "${i}" | tr '[:lower:]' '[:upper:]')]:"$'\n'"${message_craft}"$'\n'
 		else
